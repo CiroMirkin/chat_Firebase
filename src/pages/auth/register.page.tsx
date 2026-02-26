31f2e10f817a6b8ded4c3fd0dfbb93f1b3cd43f5
Ciro Mirkin <ciromirkin@gmail.com>
2026-02-26 18:19:25 -0300

    registro de usuarios con google

--- src/pages/auth/register.page.tsx
+++ src/pages/auth/register.page.tsx
@@ -1,7 +1,23 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { useAuth } from "reactfire"
 
 function RegisterPage() {
+    
+    const auth = useAuth()
+    const handleGoogleRegister = async () => {
+        try {
+            const provider = new GoogleAuthProvider()
+            await signInWithPopup(auth, provider)
+        }
+        catch(e) {
+            console.error(e)
+        }
+    }
+
     return (
-        <p>reg</p>
+        <>
+            <button onClick={handleGoogleRegister}>Registrarme con Google</button>
+        </>
     )
 }
 
