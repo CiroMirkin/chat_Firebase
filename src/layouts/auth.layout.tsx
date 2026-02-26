790e67e38c9cdfb7cc3749fc2d6a3b0cea31d889
Ciro Mirkin <ciromirkin@gmail.com>
2026-02-26 18:12:52 -0300

    redireccion dentro de las rutas auth luego de iniciar sesion

--- src/layouts/auth.layout.tsx
+++ src/layouts/auth.layout.tsx
@@ -1,7 +1,16 @@
-import { Outlet } from "react-router"
+import { Navigate, Outlet } from "react-router"
+import { useSigninCheck } from "reactfire"
 
 function AuthLayout() {
+  const { status, data: singInCheckResult, hasEmitted } = useSigninCheck()
+  
+  if(status == 'loading' || !hasEmitted) {
+    return <p>....</p>
+  }
 
+  if(status == 'success' && singInCheckResult.signedIn) {
+    return <Navigate to="/admin/" replace />
+  }
   return (
     <>
       <Outlet />
