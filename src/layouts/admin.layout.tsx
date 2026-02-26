96c4adcbc2deb1ae035a556d623a92a0eb690cbe
Ciro Mirkin <ciromirkin@gmail.com>
2026-02-26 18:10:22 -0300

    proteccion de rutas admin

--- src/layouts/admin.layout.tsx
+++ src/layouts/admin.layout.tsx
@@ -1,7 +1,17 @@
-import { Outlet } from "react-router"
+import { Navigate, Outlet } from "react-router"
+import { useSigninCheck } from "reactfire"
 
 function AdminLayout() {
+  const { status, data: singInCheckResult, hasEmitted } = useSigninCheck()
+  
+  if(status == 'loading' || !hasEmitted) {
+    return <p>....</p>
+  }
 
+  if(!singInCheckResult.signedIn) {
+    return <Navigate to="/auth/login" replace />
+  }
+  
   return (
     <>
       <Outlet />
