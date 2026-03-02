4a8776ca0b792010d2bc741ab2de6bd3ea89229f
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-01 21:33:49 -0300

    envio de mensajes dentro de useMessagesActions

--- src/hooks/useMessagesActions.tsx
+++ src/hooks/useMessagesActions.tsx
@@ -1,6 +1,6 @@
-import type { Message } from "@/schemas/roomSchema"
-import { collection, orderBy, query } from "firebase/firestore"
-import { useFirestore, useFirestoreCollectionData } from "reactfire"
+import type { LastMessage, Message } from "@/schemas/roomSchema"
+import { addDoc, collection, doc, orderBy, query, serverTimestamp, updateDoc } from "firebase/firestore"
+import { useFirestore, useFirestoreCollectionData, useUser } from "reactfire"
 
 export const useMessagesActions = (roomId: string) => {
     const db = useFirestore()
@@ -11,7 +11,32 @@ export const useMessagesActions = (roomId: string) => {
         idField: 'id',
     })
 
+    const { data: user }= useUser()
+    const sendMessage = async (messageText: string) => {
+        if(!user) throw new Error('User not found')
+        
+        const timestamp = serverTimestamp()
+        const newMessage: Omit<Message, 'id'> = {
+            senderId: user!.uid,
+            text: messageText,
+            timestamp,
+        }
+        
+        const roomRef = doc(db, "rooms", roomId)
+        const lastMessage: LastMessage = {
+            ...newMessage
+        }
+
+        await Promise.all([
+            addDoc(messagesRef, newMessage),
+            updateDoc(roomRef, {
+                lastMessage,
+            }),
+        ])
+    }
+
     return {
-        messages: messages as Message[]
+        messages: messages as Message[],
+        sendMessage,
     }
 }
