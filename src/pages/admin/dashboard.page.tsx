be1ba2557af665fcdb599d229eb95b4ff76b0a36
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-03 14:19:36 -0300

    elimina codigo muerto

--- src/pages/admin/dashboard.page.tsx
+++ src/pages/admin/dashboard.page.tsx
@@ -1,10 +1,5 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { useUser } from "reactfire"
 
 function DashboardPage() {
-    const { data: user } = useUser()
-
     return (
         <>
             <div className="p-6">
