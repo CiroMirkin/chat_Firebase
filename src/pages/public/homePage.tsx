baa1fdbaff5033dd2579cab109fe59b4ceb8268a
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-03 18:07:31 -0300

    chore: actualizar pagina de inicio

--- src/pages/public/homePage.tsx
+++ src/pages/public/homePage.tsx
@@ -6,7 +6,7 @@ function HomePage() {
         <div className="min-h-screen p-6">
             <div className="max-w-4xl mx-auto space-y-8">
                 <section className="text-center space-y-4">
-                    <h1 className="text-4xl font-bold">Firebase Chat</h1>
+                    <h1 className="text-4xl font-bold">Chat</h1>
                     <p className="text-muted-foreground text-lg">
                         Chatea en tiempo real con otros usuarios
                     </p>
