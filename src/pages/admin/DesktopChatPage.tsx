5cbfb82554982053fafe544aaf1e0d394252d672
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-13 01:11:40 -0300

    feat: implementar bloque del chat al detectar dispositivos mobiles

---
+++ src/pages/admin/DesktopChatPage.tsx
@@ -0,0 +1,42 @@
+import Chat from "@/components/Chat"
+import EmptyChat from "@/components/EmptyChat"
+import ListContact from "@/components/ListContact"
+import { Spinner } from "@/components/ui/spinner"
+import { ResizablePanel, ResizableHandle, ResizablePanelGroup } from "@/components/ui/resizable"
+import { Suspense, useContext } from "react"
+import ChatHeaderLeft from "@/components/ChatHeaderLeft"
+import ChatHeaderRight from "@/components/ChatHeaderRight"
+import FindUser from "@/components/FindUser"
+import { ChatContext } from "@/contexts/ChatContext"
+
+function DesktopChatPage(){
+    const { chatId: actualChatId, setChatId } = useContext(ChatContext)
+
+    return (
+        <ResizablePanelGroup orientation="horizontal" className="w-full h-full">
+            <ResizablePanel defaultSize="30%" minSize="11%">
+                <div className="bg-secondary/10 flex flex-col h-full">
+                    <ChatHeaderLeft />
+                    <FindUser setChatId={setChatId}/>
+                    <Suspense fallback={<div className="flex items-center justify-center h-full"><Spinner /></div>}>
+                        <ListContact chatId={actualChatId} setChatId={setChatId} />
+                    </Suspense>
+                </div>
+            </ResizablePanel>
+            <ResizableHandle withHandle className="hover:bg-primary transition-colors" />
+            <ResizablePanel defaultSize="70%" minSize="30%">
+                <div className="bg-secondary/10 flex flex-col h-full">
+                    <ChatHeaderRight selectedChatId={actualChatId} />
+                    <div className="flex-1 min-h-0">
+                        <Suspense fallback={<div className="flex items-center justify-center h-full"><Spinner /></div>}>
+                            {!actualChatId && <EmptyChat />}
+                            {actualChatId && <Chat chatId={actualChatId} />}
+                        </Suspense>
+                    </div>
+                </div>
+            </ResizablePanel>
+        </ResizablePanelGroup>
+    )
+}
+
+export default DesktopChatPage
--- src/pages/admin/chatPage.tsx
+++ src/pages/admin/chatPage.tsx
@@ -1,41 +1,22 @@
-import Chat from "@/components/Chat"
-import EmptyChat from "@/components/EmptyChat"
-import ListContact from "@/components/ListContact"
-import { Spinner } from "@/components/ui/spinner"
-import { ResizablePanel, ResizableHandle, ResizablePanelGroup } from "@/components/ui/resizable"
-import { Suspense, useContext } from "react"
-import { ChatContext } from "@/contexts/ChatContext"
-import ChatHeaderLeft from "@/components/ChatHeaderLeft"
-import ChatHeaderRight from "@/components/ChatHeaderRight"
-import FindUser from "@/components/FindUser"
+import { useMediaQuery } from "react-responsive"
+import DesktopChatPage from "./DesktopChatPage"
 
 function ChatPage() {
-    const { chatId: actualChatId, setChatId } = useContext(ChatContext)
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-width: 750px)'
+    })
+
+    if(isDesktopOrLaptop) {
+        return <DesktopChatPage />
+    }
 
     return (
-        <ResizablePanelGroup orientation="horizontal" className="w-full h-full">
-            <ResizablePanel defaultSize="30%" minSize="11%">
-                <div className="bg-secondary/10 flex flex-col h-full">
-                    <ChatHeaderLeft />
-                    <FindUser setChatId={setChatId}/>
-                    <Suspense fallback={<div className="flex items-center justify-center h-full"><Spinner /></div>}>
-                        <ListContact chatId={actualChatId} setChatId={setChatId} />
-                    </Suspense>
-                </div>
-            </ResizablePanel>
-            <ResizableHandle withHandle className="hover:bg-primary transition-colors" />
-            <ResizablePanel defaultSize="70%" minSize="30%">
-                <div className="bg-secondary/10 flex flex-col h-full">
-                    <ChatHeaderRight selectedChatId={actualChatId} />
-                    <div className="flex-1 min-h-0">
-                        <Suspense fallback={<div className="flex items-center justify-center h-full"><Spinner /></div>}>
-                            {!actualChatId && <EmptyChat />}
-                            {actualChatId && <Chat chatId={actualChatId} />}
-                        </Suspense>
-                    </div>
-                </div>
-            </ResizablePanel>
-        </ResizablePanelGroup>
+         <div className="h-screen grid place-items-center gap-2 p-4">
+            <div>
+                <h1 className="text-lg font-semibold">Lo siento, actualmente no el chat no esta disponible para moviles.</h1>
+                <p className="opacity-80 text-base">Muy pronto lo estará.</p>
+            </div>
+         </div>
     )
 }
 
